b9f3d47cc8c72c536dc5d0080eb287cf40caf6d3
Treeks <trikamitra@gmail.com>
2018-05-26 14:56:20 -0400

    added img functionality to JS

--- assets/javascript/FoodTracker.js
+++ assets/javascript/FoodTracker.js
@@ -8,16 +8,20 @@
     messagingSenderId: "277573844017"
   };
   firebase.initializeApp(config);
+  var database = firebase.database();
+
+  var food = 'margherita pizza';
 
 // For nutritional info
 $.ajax({
-    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?title=Pasta",
+    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?title=" + food,
     method: "GET",
     headers:{
         "X-Mashape-Key":"mWSYqC5gHvmshnuUYlyxmn2HId5zp1uP4wHjsnKKFlHkkIhAvq",
         "X-Mashape-Host":"spoonacular-recipe-food-nutrition-v1.p.mashape.com"
     }
   }).then(function(response) {
+    console.log('food info');
     console.log(response);
     console.log(response.calories.value);
     console.log(response.carbs.value);
@@ -26,28 +30,23 @@ $.ajax({
   });
 
 // Wine Pairing
-
-var food = "Margherita Pizza";
 $.ajax({
     url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + food + "&maxPrice=100",
     method: "GET",
     headers:{
         "X-Mashape-Key":"mWSYqC5gHvmshnuUYlyxmn2HId5zp1uP4wHjsnKKFlHkkIhAvq",
         "X-Mashape-Host":"spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-    }
-    
+    } 
   }).then(function(response) {
+    console.log('wine pairings')
     console.log(response);
     console.log(response.pairedWines)
     console.log(response.pairedWines[0])
     wineChoice = response.pairedWines[0];
     wineCall();
   });
-
   var wineChoice;
-
   //pairedWines is property
-  
   function wineCall(){
   $.ajax({
     url: 'https://lcboapi.com/products?q=' + wineChoice,
@@ -55,23 +54,32 @@ $.ajax({
     headers: { 
         'Authorization': 'Token MDo4MzRjY2I1MC02MGZiLTExZTgtODMzMS1iZmE1NDQ0YmJkZWE6TXJRWHdkYmF3TkZ1NTFlaERJYVZvdFZkakVzSlk3VWFSRzRk' }
   }).then(function(response) {
-    console.log('products');
+    console.log('LCBO wine products');
     console.log(response);
-    var firstWineChoice = response.result[0].name;
-    var secondWineChoice = response.result[1].name;
-    var thirdWineChoice = response.result[2].name;
-    console.log(firstWineChoice, secondWineChoice, thirdWineChoice);
+    var firstWineChoice = response.result[0];
+    var secondWineChoice = response.result[1];
+    var thirdWineChoice = response.result[2];
+    console.log(firstWineChoice.name, secondWineChoice.name, thirdWineChoice.name);
+    function displayWine() {
+        var wineImage = $("<img>");
+        wineImage.attr('src',firstWineChoice.image_url);
+        $('body').append(wineImage);
+    }
+    displayWine();
   });
 }
 
 //images API
     $.ajax({
-      url: 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=most_popular&phrase=spaghetti',
-      method: 'GET',
-      headers: { 
-          'Api-Key':'wyr4aumhujeqza2t6prt6u2h'}
+        url: 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=most_popular&phrase=' + food,
+        method: 'GET',
+        headers: { 
+            'Api-Key':'wyr4aumhujeqza2t6prt6u2h'}
     }).then(function(response) {
         console.log(response);
-        document.write(response.images[0].display_sizes[0].uri);
+        //adding food image to page
+        var foodImage = $("<img>");
+        foodImage.attr('src', response.images[0].display_sizes[0].uri);
+        $('body').append(foodImage); 
     });
-  
+
