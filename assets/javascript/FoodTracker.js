f35938bb652ddca6c8a8e451a5ac7c6059ed8d1a
Treeks <trikamitra@gmail.com>
2018-05-26 14:13:57 -0400

    added another API

--- assets/javascript/FoodTracker.js
+++ assets/javascript/FoodTracker.js
@@ -1,23 +1,22 @@
-var movie = "Mr. Nobody";
-var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
 // For nutritional info
 
-// $.ajax({
-//     url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?title=Pasta",
-//     method: "GET",
-//     headers:{
-//         "X-Mashape-Key":"mWSYqC5gHvmshnuUYlyxmn2HId5zp1uP4wHjsnKKFlHkkIhAvq",
-//         "X-Mashape-Host":"spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-//     }
-//   }).then(function(response) {
-//     console.log(response);
-//     console.log(response.calories.value);
-//     console.log(response.carbs.value);
-//     console.log(response.fat.value);
-//     console.log(response.protein.value);
-//   });
+$.ajax({
+    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?title=Pasta",
+    method: "GET",
+    headers:{
+        "X-Mashape-Key":"mWSYqC5gHvmshnuUYlyxmn2HId5zp1uP4wHjsnKKFlHkkIhAvq",
+        "X-Mashape-Host":"spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+    }
+  }).then(function(response) {
+    console.log(response);
+    console.log(response.calories.value);
+    console.log(response.carbs.value);
+    console.log(response.fat.value);
+    console.log(response.protein.value);
+  });
+
 // Wine Pairing
+
 var food = "Margherita Pizza";
 $.ajax({
     url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + food + "&maxPrice=100",
@@ -49,7 +48,20 @@ $.ajax({
     console.log('products');
     console.log(response);
     var firstWineChoice = response.result[0].name;
-    console.log(firstWineChoice);
+    var secondWineChoice = response.result[1].name;
+    var thirdWineChoice = response.result[2].name;
+    console.log(firstWineChoice, secondWineChoice, thirdWineChoice);
   });
 }
 
+//images API
+    $.ajax({
+      url: 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=most_popular&phrase=spaghetti',
+      method: 'GET',
+      headers: { 
+          'Api-Key':'wyr4aumhujeqza2t6prt6u2h'}
+    }).then(function(response) {
+        console.log(response);
+        document.write(response.images[0].display_sizes[0].uri);
+    });
+  
