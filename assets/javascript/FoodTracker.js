b25152b30eac5f5c11b8b707f77559696e64e326
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-05-29 20:22:50 -0400

    code for obtaining ajax response according to user meal input

--- assets/javascript/FoodTracker.js
+++ assets/javascript/FoodTracker.js
@@ -1,85 +1,117 @@
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDjOZYeHYU1gc7YLAbhuDKaKhQ8--1f1f8",
-    authDomain: "food-tracker-90c0e.firebaseapp.com",
-    databaseURL: "https://food-tracker-90c0e.firebaseio.com",
-    projectId: "food-tracker-90c0e",
-    storageBucket: "",
-    messagingSenderId: "277573844017"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
+  // var config = {
+  //   apiKey: "AIzaSyDjOZYeHYU1gc7YLAbhuDKaKhQ8--1f1f8",
+  //   authDomain: "food-tracker-90c0e.firebaseapp.com",
+  //   databaseURL: "https://food-tracker-90c0e.firebaseio.com",
+  //   projectId: "food-tracker-90c0e",
+  //   storageBucket: "",
+  //   messagingSenderId: "277573844017"
+  // };
+  // firebase.initializeApp(config);
+  // var database = firebase.database();
 
-  var food = 'margherita pizza';
+
+  var food = "";
+  var calories = "";
+  var fat = '';
+  var carbs='';
+  var protein='';
+
+
 
 // For nutritional info
-$.ajax({
-    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?title=" + food,
-    method: "GET",
-    headers:{
-        "X-Mashape-Key":"mWSYqC5gHvmshnuUYlyxmn2HId5zp1uP4wHjsnKKFlHkkIhAvq",
-        "X-Mashape-Host":"spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-    }
-  }).then(function(response) {
-    console.log('food info');
-    console.log(response);
-    console.log(response.calories.value);
-    console.log(response.carbs.value);
-    console.log(response.fat.value);
-    console.log(response.protein.value);
+
+  
+    // console.log('food info');
+    // console.log(response);
+    // console.log(response.calories.value);
+    // console.log(response.carbs.value);
+    // console.log(response.fat.value);
+    // console.log(response.protein.value);
+    $("#add-meal").on("click", function(event) {
+      event.preventDefault();
+      var food = $("#meal").val().trim();
+
+      $.ajax({
+        url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?title=" + food,
+        method: "GET",
+        headers:{
+            "X-Mashape-Key":"mWSYqC5gHvmshnuUYlyxmn2HId5zp1uP4wHjsnKKFlHkkIhAvq",
+            "X-Mashape-Host":"spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+        }
+      }).then(function(response) {
+
+      
+      console.log(food);
+      console.log(response);
+      var calories = response.calories.value;
+       var carbs = response.carbs.value;
+       var fat = response.fat.value;
+       var protein = response.protein.value;
+       console.log(calories, carbs, fat, protein);
+       $(".form-control").val("");
+
   });
+
+  })
+
+ // Capture values from text boxes
+
+
+
+
 
 // Wine Pairing
-$.ajax({
-    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + food + "&maxPrice=100",
-    method: "GET",
-    headers:{
-        "X-Mashape-Key":"mWSYqC5gHvmshnuUYlyxmn2HId5zp1uP4wHjsnKKFlHkkIhAvq",
-        "X-Mashape-Host":"spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-    } 
-  }).then(function(response) {
-    console.log('wine pairings')
-    console.log(response);
-    console.log(response.pairedWines)
-    console.log(response.pairedWines[0])
-    wineChoice = response.pairedWines[0];
-    wineCall();
-  });
-  var wineChoice;
-  //pairedWines is property
-  function wineCall(){
-  $.ajax({
-    url: 'https://lcboapi.com/products?q=' + wineChoice,
-    method: 'GET',
-    headers: { 
-        'Authorization': 'Token MDo4MzRjY2I1MC02MGZiLTExZTgtODMzMS1iZmE1NDQ0YmJkZWE6TXJRWHdkYmF3TkZ1NTFlaERJYVZvdFZkakVzSlk3VWFSRzRk' }
-  }).then(function(response) {
-    console.log('LCBO wine products');
-    console.log(response);
-    var firstWineChoice = response.result[0];
-    var secondWineChoice = response.result[1];
-    var thirdWineChoice = response.result[2];
-    console.log(firstWineChoice.name, secondWineChoice.name, thirdWineChoice.name);
-    function displayWine() {
-        var wineImage = $("<img>");
-        wineImage.attr('src',firstWineChoice.image_url);
-        $('body').append(wineImage);
-    }
-    displayWine();
-  });
-}
+// $.ajax({
+//     url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + food + "&maxPrice=100",
+//     method: "GET",
+//     headers:{
+//         "X-Mashape-Key":"mWSYqC5gHvmshnuUYlyxmn2HId5zp1uP4wHjsnKKFlHkkIhAvq",
+//         "X-Mashape-Host":"spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+//     } 
+//   }).then(function(response) {
+//     console.log('wine pairings')
+//     console.log(response);
+//     console.log(response.pairedWines)
+//     console.log(response.pairedWines[0])
+//     wineChoice = response.pairedWines[0];
+//     wineCall();
+//   });
+//   var wineChoice;
+//   //pairedWines is property
+//   function wineCall(){
+//   $.ajax({
+//     url: 'https://lcboapi.com/products?q=' + wineChoice,
+//     method: 'GET',
+//     headers: { 
+//         'Authorization': 'Token MDo4MzRjY2I1MC02MGZiLTExZTgtODMzMS1iZmE1NDQ0YmJkZWE6TXJRWHdkYmF3TkZ1NTFlaERJYVZvdFZkakVzSlk3VWFSRzRk' }
+//   }).then(function(response) {
+//     console.log('LCBO wine products');
+//     console.log(response);
+//     var firstWineChoice = response.result[0];
+//     var secondWineChoice = response.result[1];
+//     var thirdWineChoice = response.result[2];
+//     console.log(firstWineChoice.name, secondWineChoice.name, thirdWineChoice.name);
+//     function displayWine() {
+//         var wineImage = $("<img>");
+//         wineImage.attr('src',firstWineChoice.image_url);
+//         $('body').append(wineImage);
+//     }
+//     displayWine();
+//   });
+// }
 
-//images API
-    $.ajax({
-        url: 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=most_popular&phrase=' + food,
-        method: 'GET',
-        headers: { 
-            'Api-Key':'wyr4aumhujeqza2t6prt6u2h'}
-    }).then(function(response) {
-        console.log(response);
-        //adding food image to page
-        var foodImage = $("<img>");
-        foodImage.attr('src', response.images[0].display_sizes[0].uri);
-        $('body').append(foodImage); 
-    });
+// //images API
+//     $.ajax({
+//         url: 'https://api.gettyimages.com/v3/search/images?fields=id,title,thumb,referral_destinations&sort_order=most_popular&phrase=' + food,
+//         method: 'GET',
+//         headers: { 
+//             'Api-Key':'wyr4aumhujeqza2t6prt6u2h'}
+//     }).then(function(response) {
+//         console.log(response);
+//         //adding food image to page
+//         var foodImage = $("<img>");
+//         foodImage.attr('src', response.images[0].display_sizes[0].uri);
+//         $('body').append(foodImage); 
+//     });
 
