6905a4eb9bc2db4087bee3ed8b8f1c0c39e86302
Treeks <trikamitra@gmail.com>
2018-06-04 19:04:19 -0400

    minor JS and CSS changes

--- assets/javascript/FoodTracker.js
+++ assets/javascript/FoodTracker.js
@@ -20,7 +20,6 @@ $(".img-hide").hide();
 $("#pairing-text").hide();
 $("#wineIntro").hide();
 
-
 $("#add-meal").on("click", function (event) {
     event.preventDefault();
     $('html').scrollTop(0);
@@ -32,9 +31,8 @@ $("#add-meal").on("click", function (event) {
     var food = Food.toLowerCase();
     $("#meal").val("");
     if (localStorage.getItem(food + "nutrition")) {
-        console.log("this was searched for");
         var getNutrition = JSON.parse(localStorage.getItem(food + "nutrition"));
-        console.log(getNutrition);
+        // console.log(getNutrition);
         $(".table-area").show();
         $(".img-hide").show();
         $("#calories-input").text(getNutrition.calories);
@@ -42,7 +40,6 @@ $("#add-meal").on("click", function (event) {
         $("#carbohydrates-input").text(getNutrition.carbohydrates);
         $("#protein-input").text(getNutrition.protein);
         findWine();
-
     } else {
         $.ajax({
             url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/guessNutrition?title=" + food,
@@ -60,24 +57,19 @@ $("#add-meal").on("click", function (event) {
                 $(".table-area").hide();
                 $(".img-hide").hide();
                 $("#wineIntro").hide();
-
-
             } else {
                 $(".table-area").show();
                 $(".img-hide").show();
-                console.log(food);
                 console.log(response);
                 var calories = response.calories.value;
                 var carbs = response.carbs.value;
                 var fat = response.fat.value;
                 var protein = response.protein.value;
-                console.log(calories, carbs, fat, protein);
+                // console.log(calories, carbs, fat, protein);
                 $("#calories-input").text(calories);
                 $("#fat-input").text(fat);
                 $("#carbohydrates-input").text(carbs);
                 $("#protein-input").text(protein);
-
-                $("#meal").val("");
                 // Pushing meal and macro values to the database
                 var nutrition = {
                     "recipe": food,
@@ -109,24 +101,21 @@ $("#add-meal").on("click", function (event) {
             }
         }).then(function (response) {
             if (response.status === "failure" || response.pairedWines.length === 0) {
-                console.log("lol");
                 var fallBackWine = $("<p id='wineError'> Sorry, we don't have any paired wines in our database </p>");
-                console.log("lol");
                 $("#wineArea").append(fallBackWine);
                 $("#wineIntro").hide();
             } else {
-                console.log('wine pairings')
-                console.log(response);
+                // console.log(response);
                 //shows the top 3 wines for i=[0,2]
-                console.log(response.pairedWines)
-                //shows wine pairing text for whatever food
-                console.log(response.pairingText)
+                // console.log(response.pairedWines)
+                // //shows wine pairing text for whatever food
+                // console.log(response.pairingText)
                 $("#pairing-text").html(response.pairingText);
                 $('#pairing-text').show();
                 $("#wineIntro").show();
                 wineChoice = response.pairedWines;
                 for (var i = 0; i < 3; i++) {
-                    console.log(wineChoice[i]);
+                    // console.log(wineChoice[i]);
                     wineCall();
                 }
             }
@@ -139,23 +128,11 @@ $("#add-meal").on("click", function (event) {
                         'Authorization': 'Token MDo4MzRjY2I1MC02MGZiLTExZTgtODMzMS1iZmE1NDQ0YmJkZWE6TXJRWHdkYmF3TkZ1NTFlaERJYVZvdFZkakVzSlk3VWFSRzRk'
                     }
                 }).then(function (response) {
-                    var randomNumber = Math.floor(Math.random() * (19 - 0 + 1)) + 0;
-
-
+                    var randomNumber = Math.floor(Math.random() * (response.result.length - 0 + 1)) + 0;
                     console.log(response);
                     var wineLCBO = response.result[randomNumber];
-                    console.log("name: " + wineLCBO.name);
-                    console.log("varietal: " + wineLCBO.varietal);
-                    console.log("image URL: " + wineLCBO.image_url);
                     var price = (wineLCBO.price_in_cents) / 100
-                    console.log("price: $" + price);
-                    console.log("sugar in g/L: " + wineLCBO.sugar_in_grams_per_liter);
-                    console.log("package: " + wineLCBO.package);
-                    console.log("mL: " + wineLCBO.package_unit_volume_in_milliliters);
-                    console.log('tags: ' + wineLCBO.tags);
-                    console.log('style: ' + wineLCBO.style);
                     displayWine();
-
                     var newWine = { //left side - firebase, right side - var from your code
                         "type": wineLCBO.varietal,
                         "name": wineLCBO.name,
@@ -167,11 +144,8 @@ $("#add-meal").on("click", function (event) {
                         'style': wineLCBO.style,
                         'imageURL': wineLCBO.image_url
                     }
-
-
                     function displayWine() {
                         var cardCol = $("<div class='col l4 m9 offset-m1 s10 offset-s1'>")
-
                         var cardImage = $("<div class='card-image'>")
                         var onHoverInfo = $("<div class='card-reveal' id='hover-wine'>")
                         var textInfo = $("<p>");
@@ -183,12 +157,9 @@ $("#add-meal").on("click", function (event) {
                         var wineTitle = $("<span class='card-title'>");
                         wineTitle.text(wineLCBO.name);
                         cardImage.append(onHoverInfo, wineImg, wineTitle);
-
                         cardCol.append(cardImage);
-
                         $("#wineArea").append(cardCol);
                     }
-
                     //this will display each of the three wine pairings under a new folder in firebase with the title of the food var
                     database.ref('/' + food).push(newWine);
                     //adds to local storage the food and wine variety as key and name of wine as value
@@ -203,8 +174,7 @@ $("#add-meal").on("click", function (event) {
         method: 'GET',
         // key: '9191233-f0142d44da13f3353c64ec9fc'
     }).then(function (response) {
-        console.log($.ajax)
-        console.log(response);
+        // console.log(response);
         //adding food image to page
         var foodImage = $("<img>");
         var foodImageURL = response.hits[0].largeImageURL;
